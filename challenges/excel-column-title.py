020e9843e1e0a223cbd710d52d9129e363912975
Nate Dixon <njdixon23@gmail.com>
2025-09-03 18:54:36 -0400

    passed tests with 2 characters

--- challenges/excel-column-title.py
+++ challenges/excel-column-title.py
@@ -21,29 +21,56 @@ For example:
 ------ SOLUTION ------
 
 
+
+26 -> Z
+27 -> AA
+...
+52 -> 26 * 2 -> AZ
+53 -> 26 * 3 - 25 BA
+
+
+ASCII
+1 -> 64 + num = 65 = A
+2 -> 64 + num = 66 = B
+...
+27 -> int(27/26) = 1, 27 % 26 = 1 -> 1 1 = AA
+
+
+
 Time Complexity: 
 Space Complexity:
 '''
 
 def convert_to_title(num):
-    return
+
+    num1 = int(num/26)    # get leading digit (amount of alphabet rotations)
+    num2 = num % 26       # get remainder (alphabet character)
+
+    char1 = chr(64 + num1)
+    char2 = chr(64 + num2)
+
+    result = char1 + char2
+    result = result.replace("@", "")
+
+    return result
+
 
 def main():
 
     tests = {
-        "1" : "A",
-        "18" : "R",
-        "28" : "AB",
-        "52" : "AZ",
-        "701" : "ZY",
-        "229704" : "MATT",
-        "209380622941" : "ZATOICHI",
+        1 : "A",
+        18 : "R",
+        28 : "AB",
+        52 : "AZ",
+        701 : "ZY",
+        229704 : "MATT",
+        209380622941 : "ZATOICHI"
     }
 
     for test in tests:
         result = convert_to_title(test)
         if tests[test] != convert_to_title(test):
-            print("✗ - Fail\nGot " + result + " but expected" + tests[test] + ".")
+            print("✗ - Fail\nGot " + result + " but expected " + tests[test] + ".")
             continue
         print("✔ - Passed\n")
 
